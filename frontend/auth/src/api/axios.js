12e0b62691cbadb2ab0203e758860e36b48a6454
manavlimbachiya123456-star <manavlimbachiya123456@gmail.com>
2026-09-14 14:01:39 +0530

    Update axios.js

--- frontend/auth/src/api/axios.js
+++ frontend/auth/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
-
 // Automatically attach the JWT token (from login) to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
