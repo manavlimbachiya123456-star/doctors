8aa38f428cee859ef7f2a2c0784031d2be5e40bf
manavlimbachiya123456-star <manavlimbachiya123456@gmail.com>
2026-09-14 14:03:01 +0530

    Update login API URL to use environment variable

--- frontend/auth/src/pages/Login.jsx
+++ frontend/auth/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
 const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const res = await axios.post("http://localhost:3000/login", form);
+  const res = await axios.post(`${import.meta.env.VITE_API_URL || "http://localhost:3000"}/login`, form);
 
     localStorage.setItem('token', res.data.token)       // save token
     localStorage.setItem('user', JSON.stringify(res.data.user)) // save user
